1687fba520db350253f70886a210a58a56f8adf7
arturolei <artie.lei@gmail.com>
2017-12-30 20:18:51 -0800

    First commit; probably should have configured gitignore but whatever

---
+++ catalogo/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Book(models.Model):
+    author = models.ForeignKey('auth.User')
+    author_cognome=models.CharField(max_length=200)
+    author_nome= models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    publisher= models.CharField(max_length=200)
+    pub_year = models.CharField(max_length=200)
+    language= models.CharField(max_length=100)
+    translation = models.CharField(max_length=200)
+    notes = models.TextField()
+    acquisition_year = models.CharField(max_length=200)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    catalog_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def catalogare(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
