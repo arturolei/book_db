94dcf49589b6b31a29c0f251df8825479fa0c98a
arturolei <artie.lei@gmail.com>
2018-01-14 15:13:59 -0800

    Worked on template in HTML, modified view.py

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Book
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'catalogo/post_list.html')
+    books = Book.objects.order_by('created_date')
+    return render(request, 'catalogo/post_list.html',{'books': books})
