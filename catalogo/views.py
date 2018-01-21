ea2e0a462ddc94e57231029085f5b0bf02a63bf1
arturolei <artie.lei@gmail.com>
2018-01-21 14:28:29 -0800

    Added some more templates and modified views.py, bootstrap

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Book
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
     books = Book.objects.order_by('created_date')
     return render(request, 'catalogo/post_list.html',{'books': books})
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'catalogo/book_detail.html', {'book': book})
