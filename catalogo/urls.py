d675e9729dddb1a62c6c82fe21153de61b3f22eb
arturolei <artie.lei@gmail.com>
2018-01-21 15:12:03 -0800

    Updated form/entry system

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.book_detail, name='book_detail')
+    url(r'^post/(?P<pk>\d+)/$', views.book_detail, name='book_detail'),
+    url(r'^post/new/$', views.book_new, name='book_new'),
+     url(r'^post/(?P<pk>\d+)/edit/$', views.book_edit, name='book_edit'),
 ]
